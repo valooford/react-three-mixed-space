df9acebdf9d8a45bef11945f448d0f0ab35ce148
valooford <valooford@outlook.com>
2022-03-10 12:42:46 +0300

    1-setup-engine-camera

--- src/webgl/engine/index.js
+++ src/webgl/engine/index.js
@@ -1,10 +1,24 @@
 import * as THREE from "three";
 
 class Engine {
+  static cameraFov = 60;
+  static cameraZoom = 0.5;
+  static cameraNear = 0.1;
+  static cameraFar = 100;
+
   constructor({ canvas }) {
     this.renderer = new THREE.WebGLRenderer({ canvas });
 
     this.canvas = this.renderer.domElement;
+
+    const aspectRatio = canvas.clientWidth / canvas.clientHeight;
+    this.camera = new THREE.PerspectiveCamera(
+      Engine.cameraFov,
+      aspectRatio,
+      Engine.cameraNear,
+      Engine.cameraFar
+    );
+    this.camera.zoom = Engine.cameraZoom;
   }
 }
 
