b8fea81d45fde0ecef9525bcb53e7259aa021944
valooford <valooford@outlook.com>
2022-03-10 12:32:17 +0300

    1-setup-engine-canvas

--- src/webgl/engine/index.js
+++ src/webgl/engine/index.js
@@ -3,6 +3,8 @@ import * as THREE from "three";
 class Engine {
   constructor({ canvas }) {
     this.renderer = new THREE.WebGLRenderer({ canvas });
+
+    this.canvas = this.renderer.domElement;
   }
 }
 
