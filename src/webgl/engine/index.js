53f797ba232948dd812ca9713ba6af674f98a219
valooford <valooford@outlook.com>
2022-03-10 12:31:36 +0300

    1-setup-engine-renderer

--- src/webgl/engine/index.js
+++ src/webgl/engine/index.js
@@ -1,7 +1,9 @@
 import * as THREE from "three";
 
 class Engine {
-  constructor({ canvas }) {}
+  constructor({ canvas }) {
+    this.renderer = new THREE.WebGLRenderer({ canvas });
+  }
 }
 
 export default Engine;
