e8884828d74c3c3670a912661390669e50473487
valooford <valooford@outlook.com>
2022-03-11 14:51:34 +0300

    1-setup-startup-create

--- src/webgl/index.js
+++ src/webgl/index.js
@@ -1,8 +1,10 @@
 import { getContext } from "./context";
+import MainStartup from "./startup-main";
 
 class WebGLOverlayApp {
   constructor({ canvas }) {
     const context = getContext({ canvas });
+    this.startup = new MainStartup({ context });
   }
 
   destroy() {}
---
+++ src/webgl/startup-main.js
@@ -0,0 +1,7 @@
+import * as THREE from "three";
+
+class MainStartup {
+  constructor({ context }) {}
+}
+
+export default MainStartup;
