542dcdc9594191030e167c624bfd144cdc5790f0
valooford <valooford@outlook.com>
2022-03-11 15:04:51 +0300

    1-setup-startup-cube-create

--- src/webgl/scene/Model.js
+++ src/webgl/scene/Model.js
@@ -6,6 +6,10 @@ class Model extends THREE.Object3D {
 
     this._geometry = new THREE.BoxGeometry(1, 1, 1);
     this._material = new THREE.MeshPhongMaterial({ color: "lightgreen" });
+
+    this._model = new THREE.Mesh(this._geometry, this._material);
+
+    this.add(this._model);
   }
 }
 
