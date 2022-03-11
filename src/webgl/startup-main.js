7ef063076527960802e4b541d5e53baf6b0f014f
valooford <valooford@outlook.com>
2022-03-11 15:14:55 +0300

    1-setup-startup-ambient

--- src/webgl/startup-main.js
+++ src/webgl/startup-main.js
@@ -9,8 +9,11 @@ class MainStartup {
     this.engine = engine;
     this.engine.camera.position.z = 2;
 
+    const light = new THREE.AmbientLight(0xffffff, 1);
+
     this.model = new Model();
 
+    this.engine.scene.add(light);
     this.engine.scene.add(this.model);
 
     this.engine.draw();
