d5bab55d023594eff6414d5b487a741fe8b43aa7
valooford <valooford@outlook.com>
2022-03-12 14:11:38 +0300

    2-core-loop-model-animation

--- src/webgl/scene/Model.js
+++ src/webgl/scene/Model.js
@@ -12,7 +12,10 @@ class Model extends THREE.Object3D {
     this.add(this._model);
   }
 
-  update(dtime, time) {}
+  update(dtime, time) {
+    this._model.rotation.x = time * 0.001;
+    this._model.rotation.y = time * 0.001;
+  }
 }
 
 export default Model;
