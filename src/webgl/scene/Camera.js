fec184d85e520651826e54007249c5f8bc6dab63
valooford <valooford@outlook.com>
2022-03-13 23:11:29 +0300

    4-join-angle-params

--- src/webgl/scene/Camera.js
+++ src/webgl/scene/Camera.js
@@ -5,6 +5,10 @@ class Camera extends THREE.Object3D {
     super();
 
     this.camera = camera;
+    this._targetRotation = null;
+    this._savedRotation = null;
+    this._speed = 0;
+
     this.camera.position.set(0, 0.5, 4);
 
     this.rotation.x = THREE.MathUtils.degToRad(20);
@@ -12,7 +16,9 @@ class Camera extends THREE.Object3D {
     this.add(this.camera);
   }
 
-  rotateTo(angle) {}
+  rotateTo(angle) {
+    this._targetRotation = THREE.MathUtils.degToRad(angle);
+  }
 }
 
 export default Camera;
