9f452b11048770076ba2b56fce0d0d6c35b68a93
valooford <valooford@outlook.com>
2022-03-13 20:12:09 +0300

    4-join-page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import { useMemo, useReducer } from "react";
+
+import Header from "./components/Header";
+import Page from "./components/Page";
 import WebGLOverlay from "./components/WebGLOverlay";
+import { getActions, initialState, reducer, StoreContext } from "./store";
 
 const App = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const actions = useMemo(() => getActions(dispatch), []);
+
+  const { isThreeDimensional } = state;
+
   return (
-    <div>
-      <WebGLOverlay />
-    </div>
+    <StoreContext.Provider value={[state, actions]}>
+      <div>
+        {/* <WebGLOverlay /> */}
+        <Header />
+        <Page />
+      </div>
+    </StoreContext.Provider>
   );
 };
 
--- src/components/WebGLOverlay/WebGLOverlay.jsx
+++ src/components/WebGLOverlay/WebGLOverlay.jsx
@@ -1,10 +1,13 @@
 import { useEffect, useMemo, useState } from "react";
 
+import { useStoreContext } from "../../store";
 import WebGLOverlayApp from "../../webgl";
 import classes from "./style.module.css";
 
 const WebGLOverlay = () => {
   const [canvas, setCanvas] = useState(null);
+
+  const [{ angle }] = useStoreContext();
 
   const webGLOverlayApp = useMemo(
     () => (canvas ? new WebGLOverlayApp({ canvas }) : null),
