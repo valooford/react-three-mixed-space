3d48127cbe4b98351d1fca98b3f288b64946869c
valooford <valooford@outlook.com>
2022-03-13 22:59:37 +0300

    4-join-overlay-plane-divide

--- src/webgl/scene/Plane.js
+++ src/webgl/scene/Plane.js
@@ -2,6 +2,7 @@ import * as THREE from "three";
 
 class Plane extends THREE.Object3D {
   static areaScale = 0.0084;
+  static shadowScale = Plane.areaScale * 2;
 
   static areaDepth = 0.35;
 
@@ -12,11 +13,23 @@ class Plane extends THREE.Object3D {
 
     this.rotateX(THREE.MathUtils.degToRad(-90));
 
-    const geometry = new THREE.PlaneGeometry();
-    const material = new THREE.ShadowMaterial();
-    material.opacity = 0.7;
-    this._plane = new THREE.Mesh(geometry, material);
-    this._plane.receiveShadow = true;
+    {
+      const geometry = new THREE.PlaneGeometry();
+      const material = new THREE.MeshBasicMaterial({
+        transparent: true,
+        opacity: 0,
+        depthWrite: false, // prevents shadow artifacts on overlapping planes
+      });
+      this._actionPlane = new THREE.Mesh(geometry, material);
+    }
+
+    {
+      const geometry = new THREE.PlaneGeometry();
+      const material = new THREE.ShadowMaterial();
+      material.opacity = 0.7;
+      this._shadowPlane = new THREE.Mesh(geometry, material);
+      this._shadowPlane.receiveShadow = true;
+    }
 
     const raycaster = new THREE.Raycaster();
     this.onIntersect = (e) => {
@@ -26,7 +39,10 @@ class Plane extends THREE.Object3D {
       );
       raycaster.setFromCamera(mouse, camera);
 
-      const [intersectedPlane] = raycaster.intersectObject(this._plane, false);
+      const [intersectedPlane] = raycaster.intersectObject(
+        this._actionPlane,
+        false
+      );
       if (intersectedPlane) {
         const { x, z } = intersectedPlane.point;
         onIntersect(new THREE.Vector3(x, 0, z));
@@ -35,7 +51,8 @@ class Plane extends THREE.Object3D {
     this.onIntersect = this.onIntersect.bind(this);
     canvas.addEventListener("click", this.onIntersect);
 
-    this.add(this._plane);
+    this.add(this._actionPlane);
+    this.add(this._shadowPlane);
     this.add(this.centerAnchor);
   }
 
@@ -44,15 +61,35 @@ class Plane extends THREE.Object3D {
       Plane.areaScale * size.width,
       Plane.areaScale * size.height
     );
+    const shadowSize = new THREE.Vector2(
+      Plane.shadowScale * size.width,
+      Plane.shadowScale * size.height
+    );
     const center = new THREE.Vector3(
       areaSize.x / 2,
       -areaSize.y / 2,
       Plane.areaDepth
     );
 
-    this._plane.geometry.dispose();
-    this._plane.geometry = new THREE.PlaneGeometry(areaSize.x, areaSize.y);
-    this._plane.geometry.translate(areaSize.x / 2, -areaSize.y / 2, 0);
+    this._actionPlane.geometry.dispose();
+    this._actionPlane.geometry = new THREE.PlaneGeometry(
+      areaSize.x,
+      areaSize.y
+    );
+    this._actionPlane.geometry.translate(areaSize.x / 2, -areaSize.y / 2, 0);
+
+    this._shadowPlane.geometry.dispose();
+    this._shadowPlane.geometry = new THREE.PlaneGeometry(
+      shadowSize.x,
+      shadowSize.y
+    );
+    const shiftX = (shadowSize.x - areaSize.x) / 2;
+    const shiftY = (shadowSize.y - areaSize.y) / 2;
+    this._shadowPlane.geometry.translate(
+      shadowSize.x / 2 - shiftX,
+      -shadowSize.y / 2 + shiftY,
+      0
+    );
 
     this.centerAnchor.position.copy(center);
   }
