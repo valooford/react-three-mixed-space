2c374f85bd65ddef157d36ff9a37b658ee4b2817
valooford <valooford@outlook.com>
2022-03-13 23:15:49 +0300

    4-join-angle-rotate

--- src/webgl/scene/Camera.js
+++ src/webgl/scene/Camera.js
@@ -1,6 +1,8 @@
 import * as THREE from "three";
 
 class Camera extends THREE.Object3D {
+  static smoothTime = 1500;
+
   constructor({ camera }) {
     super();
 
@@ -11,9 +13,32 @@ class Camera extends THREE.Object3D {
 
     this.camera.position.set(0, 0.5, 4);
 
-    this.rotation.x = THREE.MathUtils.degToRad(20);
-
     this.add(this.camera);
+  }
+
+  update(dtime, time) {
+    if (typeof this._targetRotation === "number") {
+      const rotation = this.rotation.x;
+      const angle = this._targetRotation - rotation;
+      // if rotation not in process or new rotation started
+      if (
+        !(typeof this._savedRotation === "number") ||
+        (this._speed && this._savedRotation !== this._targetRotation)
+      ) {
+        this._savedRotation = this._targetRotation;
+        this._speed = angle / Camera.smoothTime; // rad per millisecond
+      }
+      const step = this._speed * dtime; // rad to rotate
+      // if angle is reached
+      if (!this._speed || Math.abs(angle) < Math.abs(step)) {
+        this.rotation.x = this._targetRotation;
+        this._targetRotation = null;
+        this._savedRotation = null;
+        this._speed = null;
+        return;
+      }
+      this.rotateX(step);
+    }
   }
 
   rotateTo(angle) {
