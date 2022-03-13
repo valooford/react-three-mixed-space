8c62ed3545b4874a103f23d019af352d89a008c4
valooford <valooford@outlook.com>
2022-03-13 20:34:06 +0300

    4-join-overlay-add

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ const App = () => {
   return (
     <StoreContext.Provider value={[state, actions]}>
       <div>
-        {/* <WebGLOverlay /> */}
+        {isThreeDimensional && <WebGLOverlay />}
         <Header />
         <Page />
       </div>
